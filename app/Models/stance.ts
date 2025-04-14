625bd6c7992dd77f69735d0cfb08f6a390db67bf
emmanuel-montblanc <emmanuel.montblanc@gmail.com>
2025-04-15 01:53:51 +0200

    Adding Web project

--- app/Models/stance.ts
+++
@@ -1,3 +0,0 @@
-export interface Stance {
-    name: string;
-}
--- app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainpageComponent } from './mainpage/mainpage.component';
-
-export const routes: Routes = [
-    {path: '', component: MainpageComponent}
-];
